a1404e07b64c8c9715a3b9823caf1049d4fcbf28
touhidpasha <touhidpasha552@gmail.com>
2021-09-21 16:44:55 +0530

    employee-info-stored-in-file

--- src/main/java/payrollIO/Payroll.java
+++ src/main/java/payrollIO/Payroll.java
@@ -27,45 +27,81 @@ public class Payroll {
 
 	}
 	
+	public void writeDataToFile(){
+		
+		try
+        {
+            //Writing with Bytes
+
+
+            Path path=Paths.get("/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails/data.txt");
+            String content="emp id "+id+" emp name"+name+" and salary "+salary;
+            Files.write(path, content.getBytes());
+
+
+             //Writing with List of String
+
+
+//             Path path1=Paths.get("destination\\data1.txt");
+//            
+//             List contentInList=new ArrayList<>();
+//
+//
+//             contentInList.add("Twinkle, twinkle, little star");
+//             contentInList.add("How I wonder what you are");
+//             contentInList.add("Up above the world so high");
+//             contentInList.add("Like a diamond in the sky");
+//             contentInList.add("Twinkle, twinkle little star");
+//             contentInList.add("How I wonder what you are");
+//
+//
+//             Files.write(path1, contentInList);           
+          
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    
+	}
+
 	public void fileOperations() {
-//		
-//		try {
-////			Path path=Paths.get("/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails");//creating directory
-////			Path p;
-////			p = Files.createDirectory(path);
-////			System.out.println("Directory has been created at "+p.toString());
-//	
-//		
-//			Path path1=Paths.get("/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails/data.txt");//creating file in above directory
-//            Path p1=Files.createFile(path1);
-//            System.out.println("File has been created at "+p1.toString());
-//		
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-        
-
-        String dirName = "/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails";
-
-        try {
-        	System.out.println("list of  files are");
-			Files.list(new File(dirName).toPath())
-			        .forEach(path -> {
-			            System.out.println(path);
-			        });
+		
+		try {
+			Path path=Paths.get("/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails");//creating directory
+			Path p;
+			p = Files.createDirectory(path);
+			System.out.println("Directory has been created at "+p.toString());
+	
+		
+			Path path1=Paths.get("/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails/data.txt");//creating file in above directory
+            Path p1=Files.createFile(path1);
+            System.out.println("File has been created at "+p1.toString());
+		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		
+		String dirName = "/home/touhidpasha/eclipse-workspace/payrollIO/emplyeedetails";
+
+		try {
+			System.out.println("list of  files are");
+			Files.list(new File(dirName).toPath()).forEach(path -> {
+				System.out.println(path);
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(String args[]) {
 		Payroll obj = new Payroll();
-		//obj.readInfo();
-		//obj.writeInfoToConsole();
-		obj.fileOperations();
+		 obj.readInfo();
+		// obj.writeInfoToConsole();
+		//obj.fileOperations();
+		 obj.writeDataToFile();
 	}
 }
